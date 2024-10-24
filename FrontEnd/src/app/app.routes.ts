4eb1d991a324247a154f84ca894e2a178a44ef8a
Aldayr02 <id724577@iteso.mx>
2024-10-24 12:50:35 -0600

    feat: add basic home page

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/paths/signup/signup.component';
 import { RegisterComponent } from './components/paths/register/register.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'signup', component: SignupComponent},
-    { path: 'register', component: RegisterComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
 ];
--- FrontEnd/src/app/modules/material/material.module.ts
+++ FrontEnd/src/app/modules/material/material.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule
-    ],
-    exports: [
-        MatButtonModule,
-        MatToolbarModule,
-        MatFormFieldModule,
-        MatInputModule
-    ]
+  declarations: [],
+  imports: [CommonModule],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
